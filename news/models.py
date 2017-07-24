689ff9c2a10f9efb5694c5a048cb08b9419b4908
giovanniKleinCampigoto <giovannikleincampigoto@gmail.com>
2017-07-24 14:06:29 -0300

    Add models requested on Trello
    These are the 3 models that were requested:
    Author, Subject, Article.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Subject(models.Model):
+	name = models.CharField(max_length=200)
+	color = models.CharField(max_length=7)
+
+	def __str__(self):
+		return self.name
+
+class Author(models.Model):
+	name = models.CharField(max_length=200)
+	picture = models.ImageField(upload_to='pictures/%Y%m%d')
+
+	def __str__(self):
+		return self.name
+
+	def __unicode__(self):
+		return self.name
+
+class Article(models.Model):
+	title = models.CharField(max_length=50)
+	slug = models.CharField(max_length=50)
+	author =  models.ForeignKey(Author, on_delete=models.CASCADE)
+	subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+	publish_date = models.DateTimeField(default=timezone.now)
+	hero_image = models.ImageField(upload_to='pictures/%Y%m%d')
+	text = models.TextField()
+
+	def __str__(self):
+		return self.title
+
+	def __unicode__(self):
+		return self.title
+
+	class Meta:
+		ordering = ('title',)
+
